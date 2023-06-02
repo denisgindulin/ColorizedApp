3abdb5c2bdf4eae93a7a5fce6227aa0838aab103
Denis Gindulin <denisgindulin@me.com>
2023-06-02 12:08:57 +0300

    added IBOutlets

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - IBOutlets
+    @IBOutlet var colorizedView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    
+    @IBOutlet var blueValueLabel: UILabel!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
